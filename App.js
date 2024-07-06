e42a47e488f17273377c90816da4745d72aebdbb
sadankumar <35913923+sadankumar@users.noreply.github.com>
2024-07-07 00:28:40 +0530

    nested component

--- App.js
+++ App.js
@@ -3,11 +3,11 @@
  
 //React Element is React.createElement converts element to "Object" not html element => Then render converts to the HTML element
 
-/*const heading  = React.createElement(
-    "h1", 
-    {}, 
-    "This is React"
-);*/
+const Title  = () => (
+    <h1 className="head" tabIndex="6"> 
+        React Title Component
+    </h1>
+);
 
 //jsx code will get transpilled to createElement before render to the browser. -> we are using Parcel that uses babel
 const jsxHeading = <h1 id="heading"> React app using JSX</h1>; //internally calls the React.createElement
@@ -15,13 +15,18 @@ const jsxHeading = <h1 id="heading"> React app using JSX</h1>; //internally call
 //JSX in multiple line needs to wrapo in round bracket.()
 
 //React Functional Component: It is just a function and use the jsx
-const HeadingComponent = ()=> {
-    return <h1> React Functional Component</h1>;
-};
+const HeadingComponent = ()=> (
+    <div id="container">
+        <Title/>
+         <h1 className="heading"> React Functional Component</h1>
+    </div>
+);
 const HeadingComponent1 = ()=>  <h1> React Functional Component</h1>; //same line no need to have return keyword 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+//root.render(jsxHeading);
+
+root.render(<HeadingComponent />)
 
 //React Component
