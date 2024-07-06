3c38cc5168c17d96551f7bb70089b30d443e7155
sadankumar <35913923+sadankumar@users.noreply.github.com>
2024-07-07 00:18:38 +0530

    react component App.js

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
  import React from "react";
  import ReactDOM from "react-dom/client";
  
-//React Element is React.createElement "Object" not html element => Then render converts to the HTML element
+//React Element is React.createElement converts element to "Object" not html element => Then render converts to the HTML element
 
-const heading  = React.createElement(
+/*const heading  = React.createElement(
     "h1", 
     {}, 
     "This is React"
-);
+);*/
+
+//jsx code will get transpilled to createElement before render to the browser. -> we are using Parcel that uses babel
+const jsxHeading = <h1 id="heading"> React app using JSX</h1>; //internally calls the React.createElement
+//JSX is not html. attrubites must be in camelCase
+//JSX in multiple line needs to wrapo in round bracket.()
+
+//React Functional Component: It is just a function and use the jsx
+const HeadingComponent = ()=> {
+    return <h1> React Functional Component</h1>;
+};
+const HeadingComponent1 = ()=>  <h1> React Functional Component</h1>; //same line no need to have return keyword 
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
+
+//React Component
